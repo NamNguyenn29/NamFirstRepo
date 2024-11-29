6866922e692ba31fbdc7cf11ab81dc27bb0f8328
NamNguyenn29 <nam.nguyentan.cit22@eiu.edu.vn>
2024-11-29 17:54:57 +0700

    update print-name

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ package NamFirstRepo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Nam's Java Repository");
+        System.out.println("Hello Nam");
+        System.out.println("Welcome to my repository");
+        
     }
 }
